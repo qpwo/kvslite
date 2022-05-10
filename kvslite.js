bcb2ac3a97da93b05fd928ede13df5c803b72106
Luke Harold Miles <lukem@sent.com>
2022-05-10 01:24:34 -0700

    add collection names and getMany, setMany

--- kvslite.js
+++ kvslite.js
@@ -2,52 +2,61 @@ const SQLite3 = require('better-sqlite3')
 const { deserialize, serialize } = require('v8')
 
 class KVS {
-    constructor(dbpath) {
+    constructor(dbpath, collection = 'items') {
+        const coll = collection
         const db = new SQLite3(dbpath)
         this.db = db
         db.prepare(
-            `CREATE TABLE IF NOT EXISTS items(
-                     key   TEXT PRIMARY KEY,
-                     value BLOB)`
+            `CREATE TABLE IF NOT EXISTS ${coll} ( key TEXT PRIMARY KEY, value BLOB )`
         ).run()
 
-        this.stmt_get = db.prepare('SELECT * FROM items WHERE key = ?')
-        this.stmt_insert = db.prepare(
-            // 'INSERT INTO items (key, value) VALUES(?, ?) ON DUPLICATE KEY UPDATE'
-            'REPLACE INTO items (key, value) VALUES(?, ?)'
+        this.dbGet = db.prepare(`SELECT * FROM ${coll} WHERE key = ?`)
+        this.dbInsert = db.prepare(
+            `REPLACE INTO ${coll} (key, value) VALUES(?, ?)`
         )
-        this.stmt_all = db.prepare('SELECT * FROM items')
-        this.stmt_find = db.prepare('SELECT * FROM items WHERE key LIKE ?')
-        this.stmt_delete = db.prepare('DELETE FROM items WHERE key = ?')
+        this.dbAll = db.prepare(`SELECT * FROM ${coll}`)
+        this.dbFind = db.prepare(`SELECT * FROM ${coll} WHERE key LIKE ?`)
+        this.dbDelete = db.prepare(`DELETE FROM ${coll} WHERE key = ?`)
+        this.dbInsertMany = db.transaction(pairs => {
+            for (const [key, value] of pairs) this.dbInsert.run(key, value)
+        })
+        this.dbGetMany = db.transaction(keys =>
+            keys.map(key => this.dbGet.get(key))
+        )
     }
+
     close() {
         this.db.close()
     }
 
     get(key) {
-        const row = this.stmt_get.get(key)
-        if (!row || !row.value) {
-            return undefined
-        }
+        const row = this.dbGet.get(key)
+        if (!row) return row
         return deserialize(row.value)
     }
 
     set(key, value) {
-        this.stmt_insert.run(key, serialize(value))
+        this.dbInsert.run(key, serialize(value))
+    }
+
+    setMany(pairs) {
+        this.dbInsertMany(pairs)
+    }
+
+    getMany(keys) {
+        return this.dbGetMany(keys)
     }
 
     delete(key) {
-        this.stmt_delete.run(key)
+        this.dbDelete.run(key)
     }
 
     all() {
-        return this.stmt_all.run()
-        // return rows2obj(rows)
+        return this.dbAll.run()
     }
 
     find(prefix) {
-        return this.stmt_find.run(prefix + '%')
-        // return rows2obj(rows)
+        return this.dbFind.run(prefix + '%')
     }
 }
 
